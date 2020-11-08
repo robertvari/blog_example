7a6eb7d47c2309f6aa56cda51783079a7f556eb2
Róbert Vári <mail.robertvari@gmail.com>
2020-11-08 17:46:33 +0100

    Blog model

--- blog_api/api/models.py
+++ blog_api/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    card_body = models.TextField(max_length=1000)
+    body = models.TextField(max_length=10000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

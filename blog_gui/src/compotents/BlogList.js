99d26e34c46a98d753f9ce619c43650391d4d4c2
Róbert Vári <mail.robertvari@gmail.com>
2020-11-08 18:23:55 +0100

    BlogList useEffect

--- blog_gui/src/compotents/BlogList.js
+++ blog_gui/src/compotents/BlogList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BlogCard from "./BlogCard";
-import {Mock_BlogList} from "../Mock_BlogList";
+
 
 function BlogList(props) {
+    const [posts, set_posts] = useState([])
+
+    const fetch_posts = () => {
+        console.log("Get post list from API")
+    }
+
+    useEffect(() =>{
+        fetch_posts()
+    }, [])
+
     return (
         <div>
             {
-               Mock_BlogList.map(data => <BlogCard key={data.id} blog_data={data}/>)
+               posts.map(data => <BlogCard key={data.id} blog_data={data}/>)
             }
         </div>
     );
